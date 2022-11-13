084ff8ddcc8290cfebc695ba80f1f037fd0e98f7
natsuokawai <kawai.natsuo@gmail.com>
2022-11-13 11:33:44 +0900

    Add + and - operators

--- main.rb
+++ main.rb
@@ -1,14 +1,31 @@
+require 'strscan'
+
 if ARGV.size != 1
   STDERR.puts "#{ARGV.size}: invalid number of arguments\n"
   return
 end
 
-if ARGV[0].is_a? Integer
-  STDERR.puts "#{ARGV[0]}: invalid number og arguments\n"
-  return
-end
+s = StringScanner.new ARGV[0]
 
 puts "  .global main"
 puts "main:"
-puts "  mov $#{ARGV[0]}, %rax"
+puts "  mov $#{s.scan(/\d+/)}, %rax"
+
+until s.eos?
+  if s.check('+')
+    s.getch
+    puts "  add $#{s.scan(/\d+/)}, %rax"
+    next
+  end
+
+  if s.check('-')
+    s.getch
+    puts "  sub $#{s.scan(/\d+/)}, %rax"
+    next
+  end
+
+  STDERR.puts "unexpected character: '#{s.getch}'"
+  return
+end
+
 puts "  ret"
